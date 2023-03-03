fd3ea63f09f0a909c0189fa5680cfd384963b47e
Tim Smith <tim.smith2814@gmail.com>
2023-03-03 15:29:06 +0000

    Filter explanation

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,12 @@
 import HornedBeast from "./HornedBeast";
 
 export default function Main({ data, handleModal, hornsFilter }) {
-  // if hornsFilter is "", then don't filter
-  // if its a number, then only show the horned beasts that have that many horns
-  // use the hornsFilter prop
-
+  // .filter is an array method that takes a function as its argument
+  // the function looks for a conditional (in this case beast.horns === parseInt(hornsFilter))
+  // when the condition matches it makes the array only items that match
+  // we put:
+  // !hornsFilter ||
+  // in front of the condition so we ONLY look at that when hornsFilter isn't blank
   const filteredData = data.filter((beast) => !hornsFilter || beast.horns === parseInt(hornsFilter));
 
   return (
