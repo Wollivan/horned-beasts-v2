03062d9b2b204c5075addc8d6b6f09dd28dd4a6d
Tim Smith <tim.smith2814@gmail.com>
2023-03-03 15:26:14 +0000

    Add filter

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,15 @@
 import HornedBeast from "./HornedBeast";
 
 export default function Main({ data, handleModal, hornsFilter }) {
+  // if hornsFilter is "", then don't filter
+  // if its a number, then only show the horned beasts that have that many horns
+  // use the hornsFilter prop
+
+  const filteredData = data.filter((beast) => !hornsFilter || beast.horns === parseInt(hornsFilter));
+
   return (
     <main>
-      {data.map((beast) => {
+      {filteredData.map((beast) => {
         return (
           <HornedBeast
             title={beast.title}
